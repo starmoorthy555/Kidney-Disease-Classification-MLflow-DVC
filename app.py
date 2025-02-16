42fcd101e08bcb78311960aa0bfcfc02c80aa8cc
Moorthy. P <starmoorthy55@gmil.com>
2025-02-16 09:11:50 -0800

    prediction

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 import os
 from flask_cors import CORS, cross_origin
-from cnnClassifier.utils.common import decodeImage
+from cnnClassifier.utils.comman import decodeImage
 from cnnClassifier.pipeline.prediction import PredictionPipeline
 
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templets')
 CORS(app)
 
 
